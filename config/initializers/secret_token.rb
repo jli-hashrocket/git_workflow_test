e8c0c3d197140b5f040c2f80ee96d36978f98c68
macheteshuffle <jeff.li029@gmail.com>
2014-01-02 00:17:30 -0500

    added more gems and created db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitWorkflowTest::Application.config.secret_key_base = '3d1bd6bfa7759b616e676d4a3f14febadc879c1ec323353b3c6476762141c747e5f1506943745c0c1515503f52b9c5166f250f0d90e92fad1f3a79e9e7c28a2c'
